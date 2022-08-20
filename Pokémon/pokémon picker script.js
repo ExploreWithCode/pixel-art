6aec344181a3d3a448daec46bbc2a0841142b712
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2022-08-21 01:24:18 +0300

    Create pokémon picker script.js

---
+++ Pokémon/pokémon picker script.js
@@ -0,0 +1,17 @@
+//Use this if you want external JS instead of the "script" tag in HTML
+/* FORM
+function pokémonNameInfo() {
+  setTimeout(function(){ alert(" Name:	Name\n National Pokédex number:	#number\n Type:	type\n Gender:	♂ Male / ♀ Female"); }, 0);
+  }
+function displayImage(){
+        document.getElementById('Pokémon').src="";
+  }
+*/
+//BULBASAUR example
+function bulbasaurInfo() {
+  setTimeout(function(){ alert(" Name:	Bulbasaur\n National Pokédex number:	#1\n Type:	Grass / Poison\n Gender:	♂ Male / ♀ Female"); }, 0);//"\n" changes line of text (string)
+  }
+
+function displayImage(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/182645143-93e9546c-1276-4e15-ac74-bd58c25b346d.png";
+    }
