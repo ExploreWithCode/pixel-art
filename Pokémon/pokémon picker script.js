b564b67ed514922d19a66f611b4d48863e08db2e
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2022-08-21 15:34:59 +0300

    Update pokémon picker script.js

--- Pokémon/pokémon picker script.js
+++ Pokémon/pokémon picker script.js
@@ -1,5 +1,6 @@
 //Use this if you want external JS instead of the "script" tag in HTML
 /* FORM
+//NAME
 function pokémonNameInfo() {
   setTimeout(function(){ alert(" Name:	Name\n National Pokédex number:	#number\n Type:	type\n Gender:	♂ Male / ♀ Female"); }, 0);
   }
@@ -7,11 +8,59 @@ function displayImage(){
         document.getElementById('Pokémon').src="";
   }
 */
-//BULBASAUR example
+//BULBASAUR
 function bulbasaurInfo() {
-  setTimeout(function(){ alert(" Name:	Bulbasaur\n National Pokédex number:	#1\n Type:	Grass / Poison\n Gender:	♂ Male / ♀ Female"); }, 0);//"\n" changes line of text (string)
+  setTimeout(function(){ alert(" Name:	Bulbasaur\n National Pokédex number:	#1\n Type:	Grass / Poison\n Gender:	♂ Male / ♀ Female"); }, 0);//"\n" changes the line of text (string)
   }
-
-function displayImage(){
+function displayBulbasaur(){
         document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/182645143-93e9546c-1276-4e15-ac74-bd58c25b346d.png";
     }
+//CHARMANDER
+function charmanderInfo() {
+  setTimeout(function(){ alert(" Name:	Charmander\n National Pokédex number:	#4\n Type:	Fire\n Gender:	♂ Male / ♀ Female"); }, 0);
+  }
+function displayCharmander(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/182645137-6fdfab30-3986-4769-bb17-687fabb8571d.png";
+  }
+//EEVEE
+function eeveeInfo() {
+  setTimeout(function(){ alert(" Name:	Eevee\n National Pokédex number:	#133\n Type:	Normal\n Gender:	♂ Male / ♀ Female"); }, 0);
+  }
+function displayEevee(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/185455243-b9cf7aa5-5b17-420b-9494-8720b6676eab.png";
+  }
+//FLAREON
+function flareonInfo() {
+  setTimeout(function(){ alert(" Name:	Flareon\n National Pokédex number:	#136\n Type:	Fire\n Gender:	♂ Male / ♀ Female"); }, 0);
+  }
+function displayFlareon(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/184756655-d8632ee3-ceb1-4aea-89a1-f585c81a255e.png";
+  }
+//MEW
+function mewInfo() {
+  setTimeout(function(){ alert(" Name:	Mew\n National Pokédex number:	#151\n Type:	Psychic\n Gender:	♂ Male / ♀ Female"); }, 0);
+  }
+function displayMew(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/184548973-d050a180-45d4-433d-ae19-e3a81bcb8bf0.png";
+  }
+//MEWTWO
+function mewtwoInfo() {
+  setTimeout(function(){ alert(" Name:	Mewtwo\n National Pokédex number:	#150\n Type:	Psychic\n Gender:	♂ Male / ♀ Female"); }, 0);
+  }
+function displayMewtwo(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/183269522-1178a09a-8d59-45b6-90e9-616ddec1098a.png";
+  }
+//PIKACHU
+function pikachuInfo() {
+  setTimeout(function(){ alert(" Name:	Pikachu\n National Pokédex number:	#25\n Type:	Electric\n Gender:	♂ Male / ♀ Female"); }, 0);
+  }
+function displayPikachu(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/182130279-8cc77124-9d42-440c-a98f-24385e31247a.png";
+  }
+//VAPOREON
+function vaporeonInfo() {
+  setTimeout(function(){ alert(" Name:	Vaporeon\n National Pokédex number:	#134\n Type:	Water\n Gender:	♂ Male / ♀ Female"); }, 0);
+  }
+function displayVaporeon(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/184548331-c87a476c-18bc-452c-b463-589dda31eabe.png";
+  }
