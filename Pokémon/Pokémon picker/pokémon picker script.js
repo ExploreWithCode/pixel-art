d4f85fa66138dd3b10c0b0dabf85e622846f6a30
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2022-09-08 01:54:13 +0300

    Update pokémon picker script.js

--- Pokémon/Pokémon picker/pokémon picker script.js
+++ Pokémon/Pokémon picker/pokémon picker script.js
@@ -2,7 +2,7 @@
 
 //NAME
 function pokémonNameInfo() {
-  document.getElementById("Info").innerHTML = ' Name: \"Name\", National Pokédex number: \"#number\", Type: \"Type\", Gender: \"♂ Male / ♀ Female\" ';
+  document.getElementById("Info").innerHTML = ' Name: \"Name\", National Pokédex number: \"#number\", Type:	\"Type\", Gender: \"♂ Male / ♀ Female\" ';
   }
 function displayImage(){
         document.getElementById('Pokémon').src="";
@@ -16,27 +16,6 @@ function bulbasaurInfo() {
 function displayBulbasaur(){
         document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/182645143-93e9546c-1276-4e15-ac74-bd58c25b346d.png";
   }
-//CHARMANDER
-function charmanderInfo() {
-  document.getElementById("Info").innerHTML = ' Name: \"Charmander\", National Pokédex number: \"#4\", Type: \"Fire\", Gender: \"♂ Male / ♀ Female\" ';
-  }
-function displayCharmander(){
-        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/182645137-6fdfab30-3986-4769-bb17-687fabb8571d.png";
-  }
-//EEVEE
-function eeveeInfo() {
-  document.getElementById("Info").innerHTML = ' Name: \"Eevee\", National Pokédex number: \"#133\", Type: \"Normal\", Gender: \"♂ Male / ♀ Female\" ';
-  }
-function displayEevee(){
-        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/185455243-b9cf7aa5-5b17-420b-9494-8720b6676eab.png";
-  }
-//FLAREON
-function flareonInfo() {
-  document.getElementById("Info").innerHTML = ' Name: \"Flareon\", National Pokédex number: \"#136\", Type:	\"Fire\", Gender: \"♂ Male / ♀ Female\" ';
-  }
-function displayFlareon(){
-        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/184756655-d8632ee3-ceb1-4aea-89a1-f585c81a255e.png";
-  }
 //IVYSAUR
 function ivysaurInfo() {
   document.getElementById("Info").innerHTML = ' Name: \"Ivysaur\", National Pokédex number: \"#2\", Type: \"Grass / Poison\", Gender: \"♂ Male / ♀ Female\" ';
@@ -44,19 +23,19 @@ function ivysaurInfo() {
 function displayIvysaur(){
         document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/186333807-15538d93-20d8-4e9c-b2c3-e97d043fbc65.png";
   }
-//MEW
-function mewInfo() {
-  document.getElementById("Info").innerHTML = ' Name: \"Mew\", National Pokédex number: \"#151\", Type: \"Psychic\", Gender: \"♂ Male / ♀ Female\" ';
+//VENUSAUR
+function venusaurInfo() {
+  document.getElementById("Info").innerHTML = ' Name: \"Venusaur\", National Pokédex number: \"#3\", Type: \"Grass / Poison\", Gender: \"♂ Male / ♀ Female\" ';
   }
-function displayMew(){
-        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/184548973-d050a180-45d4-433d-ae19-e3a81bcb8bf0.png";
+function displayVenusaur(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/188745563-b1f30d38-7052-4b21-9335-cf21f1d070a8.png";
   }
-//MEWTWO
-function mewtwoInfo() {
-  document.getElementById("Info").innerHTML = ' Name: \"Mewtwo\", National Pokédex number: \"#150\", Type: \"Psychic\", Gender: \"♂ Male / ♀ Female\" ';
+//CHARMANDER
+function charmanderInfo() {
+  document.getElementById("Info").innerHTML = ' Name: \"Charmander\", National Pokédex number: \"#4\", Type: \"Fire\", Gender: \"♂ Male / ♀ Female\" ';
   }
-function displayMewtwo(){
-        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/183269522-1178a09a-8d59-45b6-90e9-616ddec1098a.png";
+function displayCharmander(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/182645137-6fdfab30-3986-4769-bb17-687fabb8571d.png";
   }
 //PIKACHU
 function pikachuInfo() {
@@ -65,12 +44,40 @@ function pikachuInfo() {
 function displayPikachu(){
         document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/182130279-8cc77124-9d42-440c-a98f-24385e31247a.png";
   }
+//EEVEE
+function eeveeInfo() {
+  document.getElementById("Info").innerHTML = ' Name: \"Eevee\", National Pokédex number: \"#133\", Type: \"Normal\", Gender: \"♂ Male / ♀ Female\" ';
+  }
+function displayEevee(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/185455243-b9cf7aa5-5b17-420b-9494-8720b6676eab.png";
+  }
 //VAPOREON
 function vaporeonInfo() {
   document.getElementById("Info").innerHTML = ' Name: \"Vaporeon\", National Pokédex number: \"#134\", Type: \"Water\", Gender:	\"♂ Male / ♀ Female\" ';
   }
 function displayVaporeon(){
         document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/184548331-c87a476c-18bc-452c-b463-589dda31eabe.png";
+  }
+//FLAREON
+function flareonInfo() {
+  document.getElementById("Info").innerHTML = ' Name: \"Flareon\", National Pokédex number: \"#136\", Type:	\"Fire\", Gender: \"♂ Male / ♀ Female\" ';
+  }
+function displayFlareon(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/184756655-d8632ee3-ceb1-4aea-89a1-f585c81a255e.png";
+  }
+//MEWTWO
+function mewtwoInfo() {
+  document.getElementById("Info").innerHTML = ' Name: \"Mewtwo\", National Pokédex number: \"#150\", Type: \"Psychic\", Gender: \"♂ Male / ♀ Female\" ';
+  }
+function displayMewtwo(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/183269522-1178a09a-8d59-45b6-90e9-616ddec1098a.png";
+  }
+//MEW
+function mewInfo() {
+  document.getElementById("Info").innerHTML = ' Name: \"Mew\", National Pokédex number: \"#151\", Type: \"Psychic\", Gender: \"♂ Male / ♀ Female\" ';
+  }
+function displayMew(){
+        document.getElementById('Pokémon').src="https://user-images.githubusercontent.com/109108536/184548973-d050a180-45d4-433d-ae19-e3a81bcb8bf0.png";
   }
 
             //  MEGA
