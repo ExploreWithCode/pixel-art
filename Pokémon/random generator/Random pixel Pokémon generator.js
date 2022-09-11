795492f627b05eb23c7f6158fd23f9ed3ae9c17f
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2022-09-11 22:43:35 +0300

    Update Random pixel Pokémon generator.js

--- Pokémon/random generator/Random pixel Pokémon generator.js
+++ Pokémon/random generator/Random pixel Pokémon generator.js
@@ -39,7 +39,7 @@ function getRandomPokémon() {
 	
 	var randomMega = new Array();	//MEGA
 	
-	randomMega[0] = "https://user-images.githubusercontent.com/109108536/189545695-1a76055d-1aaf-492c-821c-4be0fa2385f3.png";//Mega Venusaur
+	randomMega[0] = "https://user-images.githubusercontent.com/109108536/189545695-1a76055d-1aaf-492c-821c-4be0fa2385f3.png;//Mega Venusaur
 	
 	var infoMega = new Array();
 	
